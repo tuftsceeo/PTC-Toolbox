357a23fa86fe083cacdaa9b7a1ef6b41cf66961b
CarterWS <cartersilvey@gmail.com>
2020-07-16 16:17:43 -0400

    Remove Duplicates

--- vuforia-spatial-edge-server/addons/vuforia-spatial-robotic-addon/interfaces/Spike-Prime/index.js
+++ vuforia-spatial-edge-server/addons/vuforia-spatial-robotic-addon/interfaces/Spike-Prime/index.js
@@ -99,6 +99,11 @@ function startHardwareInterface() {
     server.addNode(objectName, TOOL_NAME, "motor2", "node", {x: 125, y: -25, scale:0.175});
     server.addNode(objectName, TOOL_NAME, "motor3", "node", {x: 125, y: 50, scale:0.175});
 
+    // Removes nodes that are only found in beginner
+    server.removeNode(objectName, TOOL_NAME, "LED")
+    server.removeNode(objectName, TOOL_NAME, "screen")
+    server.removeNode(objectName, TOOL_NAME, "motors")    
+
     if (complexity == 'beginner' || complexity == 'intermediate') {
         // Remove the accelerometer/gyroscope nodes
         server.removeNode(objectName, TOOL_NAME, "accelerometerX")
