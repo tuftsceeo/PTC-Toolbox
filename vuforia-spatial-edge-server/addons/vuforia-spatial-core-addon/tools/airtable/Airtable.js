affcd79db9a992dd3e839c8607d8dea5d8383de7
CarterWS <47007986+CarterWS@users.noreply.github.com>
2020-09-24 11:13:44 -0400

    Update Airtable.js

--- vuforia-spatial-edge-server/addons/vuforia-spatial-core-addon/tools/airtable/Airtable.js
+++ vuforia-spatial-edge-server/addons/vuforia-spatial-core-addon/tools/airtable/Airtable.js
@@ -1,3 +1,3 @@
-const APIKEY1 = "keyyouv7t2HfraBcn";
-const BASEID1 = "appLDvFK5jHDvyz6M";
-const TABLENAME1 = "Table 1";
+const APIKEY1 = "XXXXXXX";
+const BASEID1 = "XXXXXXX";
+const TABLENAME1 = "XXXXXXX";
